d4953ab2a7e4c0eb289db797f6ac661939f78bf6
Tham Cheng Yong <25361260+cytham@users.noreply.github.com>
2021-10-18 18:12:02 +0800

    Update variantmap_app.py

--- variantmap_app.py
+++ variantmap_app.py
@@ -18,8 +18,10 @@ import io
 
 import math
 import pandas as pd
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html
+# import dash_html_components as html
+from dash import dcc
+# import dash_core_components as dcc
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 
